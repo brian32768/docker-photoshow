5744c203f3f4df14056acd45a2c7cd83c1778e9b
bwilsoncc <bwilsoncc@co.clatsop.or.us>
2020-12-16 13:25:10 -0800

    be less talky

--- healthcheck_photoshow.sh
+++ healthcheck_photoshow.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# First, let's make sure there are some images to serve
-
 HOME="/media/photoshow"
 
+# Check volumes are mounted by looking for files
 # There are currently 3 folders mounted via CIFS and there have to be a few files in each of them.
-
+#
 for folder in $HOME/*; do
     if [ `find \$folder -type f |wc -l` -lt 5 ] ; then
 	echo `hostname` problem in $folder
@@ -18,6 +17,6 @@ done
 # -L causes curl to follow the redirect
 # so it checks on both the proxy and on the photoshow server
 
-curl -L --fail -s http://giscache.co.clatsop.or.us/photoshow/ || echo `hostname` photoshow webserver not running || exit 1
+curl -L --fail -s http://giscache.co.clatsop.or.us/photoshow/ > /dev/null || echo `hostname` photoshow webserver not running || exit 1
 
 exit 0
